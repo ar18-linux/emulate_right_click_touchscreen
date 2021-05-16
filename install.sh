b46a308ccbedb5ddd83668d993810deffb645dfc
nulysses <adr.zivil@gmail.com>
2021-05-16 12:48:10 +0200

    1

--- install.sh
+++ install.sh
@@ -32,6 +32,6 @@ pip3 install pynput
 if [ -d "/home/${user_name}/.config/ar18/autostarts" ]; then
   auto_start="/home/${user_name}/.config/ar18/autostarts/${module_name}.sh"
   cp "${script_dir}/${module_name}/startup.sh" "${auto_start}"
-  echo "${install_dir}/${module_name}/${module_name}.py" >> "${auto_start}"
+  echo "LD_PRELOAD='' ${install_dir}/${module_name}/${module_name}.py" >> "${auto_start}"
   chmod +x "${auto_start}"
 fi
